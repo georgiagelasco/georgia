887c56a97263632710cecb366e08f87494ddad7e
georgiagelasco <124117294+georgiagelasco@users.noreply.github.com>
2024-12-10 00:22:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -176,88 +176,117 @@ function updateBarChart(attribute) {
 
 // Update Heatmap
 function updateHeatmap() {
-    // set the dimensions and margins of the graph
-var margin = {top: 30, right: 30, bottom: 30, left: 100},
-  width = 450 - margin.left - margin.right,
-  height = 450 - margin.top - margin.bottom;
+   var margin = { top: 30, right: 30, bottom: 50, left: 100 },
+        width = 450 - margin.left - margin.right,
+        height = 450 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
-var svg = d3.select("#my_dataviz")
-.append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-.append("g")
-  .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")")
-;
-//Read the data
-d3.csv("covid.csv", function(data) {
-// Labels of row and columns
-var myGroups = Array.from(new Set(data.map(d => d.age_group)));
-var myVars = Array.from(new Set(data.map(d => d.race_ethnicity_combined)));
+    // Clear any existing SVG content to prevent duplication
+    d3.select("#my_dataviz").select("svg").remove();
 
-// Build X scales and axis:
-var x = d3.scaleBand()
-  .range([ 0, width ])
-  .domain(myGroups)
-  .padding(0.01);
-svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x))
+    // Append the SVG object to the page
+    var svg = d3.select("#my_dataviz")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// Build X scales and axis:
-var y = d3.scaleBand()
-  .range([ height, 0 ])
-  .domain(myVars)
-  .padding(0.01);
-svg.append("g")
-  .call(d3.axisLeft(y));
+    // Read the data
+    d3.csv("covid.csv", function (data) {
+        // Filter and prepare the data
+        data = data.filter(d => d.hosp_yn === "Yes" || d.hosp_yn === "No"); // Focus only on hospitalization data
 
-// Build color scale
-var myColor = d3.scaleLinear()
-  .range(["white", "#69b3a2"])
-  .domain([1,100])
+        // Group data by age group and race/ethnicity, counting occurrences
+        var heatmapData = d3.rollup(
+            data,
+            v => v.length,
+            d => d.age_group,
+            d => d.race_ethnicity_combined
+        );
 
+        // Extract unique age groups (Y-axis) and race/ethnicity (X-axis)
+        var ageGroups = Array.from(heatmapData.keys());
+        var raceEthnicities = Array.from(
+            new Set(data.map(d => d.race_ethnicity_combined))
+        );
 
+        // Build X scales and axis
+        var x = d3.scaleBand()
+            .range([0, width])
+            .domain(raceEthnicities)
+            .padding(0.01);
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x))
+            .selectAll("text")
+            .attr("transform", "rotate(-45)")
+            .style("text-anchor", "end");
 
-  // create a tooltip
-  var tooltip = d3.select("#my_dataviz")
-    .append("div")
-    .style("opacity", 0)
-    .attr("class", "tooltip")
-    .style("background-color", "white")
-    .style("border", "solid")
-    .style("border-width", "2px")
-    .style("border-radius", "5px")
-    .style("padding", "5px")
+        // Build Y scales and axis
+        var y = d3.scaleBand()
+            .range([height, 0])
+            .domain(ageGroups)
+            .padding(0.01);
+        svg.append("g").call(d3.axisLeft(y));
 
-  // Three function that change the tooltip when user hover / move / leave a cell
-  var mouseover = function(d) {
-    tooltip.style("opacity", 1)
-  }
-  var mousemove = function(d) {
-    tooltip
-      .html("The exact value of<br>this cell is: " + d.value)
-      .style("left", (d3.mouse(this)[0]+70) + "px")
-      .style("top", (d3.mouse(this)[1]) + "px")
-  }
-  var mouseleave = function(d) {
-    tooltip.style("opacity", 0)
-  }
+        // Build color scale
+        var maxValue = d3.max(Array.from(heatmapData.values()), d =>
+            d3.max(Array.from(d.values()))
+        );
+        var myColor = d3.scaleLinear()
+            .range(["white", "#69b3a2"])
+            .domain([0, maxValue]);
 
-  // add the squares
-  svg.selectAll()
-    .data(data, function(d) {return d.group+':'+d.variable;})
-    .enter()
-    .append("rect")
-      .attr("x", function(d) { return x(d.group) })
-      .attr("y", function(d) { return y(d.variable) })
-      .attr("width", x.bandwidth() )
-      .attr("height", y.bandwidth() )
-      .style("fill", function(d) { return myColor(d.value)} )
-    .on("mouseover", mouseover)
-    .on("mousemove", mousemove)
-    .on("mouseleave", mouseleave)
-})
+        // Create tooltip
+        var tooltip = d3.select("#my_dataviz")
+            .append("div")
+            .style("opacity", 0)
+            .attr("class", "tooltip")
+            .style("background-color", "white")
+            .style("border", "solid")
+            .style("border-width", "2px")
+            .style("border-radius", "5px")
+            .style("padding", "5px");
+
+        // Tooltip functions
+        var mouseover = function (event, d) {
+            tooltip.style("opacity", 1);
+        };
+        var mousemove = function (event, d) {
+            tooltip
+                .html(
+                    "Age Group: " + d.age_group +
+                    "<br>Race/Ethnicity: " + d.race_ethnicity +
+                    "<br>Count: " + d.value
+                )
+                .style("left", (event.pageX + 10) + "px")
+                .style("top", (event.pageY - 10) + "px");
+        };
+        var mouseleave = function (event, d) {
+            tooltip.style("opacity", 0);
+        };
+
+        // Add squares
+        svg.selectAll()
+            .data(
+                Array.from(heatmapData.entries()).flatMap(([age_group, races]) =>
+                    Array.from(races.entries()).map(([race_ethnicity, value]) => ({
+                        age_group,
+                        race_ethnicity,
+                        value
+                    }))
+                )
+            )
+            .enter()
+            .append("rect")
+            .attr("x", d => x(d.race_ethnicity))
+            .attr("y", d => y(d.age_group))
+            .attr("width", x.bandwidth())
+            .attr("height", y.bandwidth())
+            .style("fill", d => myColor(d.value))
+            .on("mouseover", mouseover)
+            .on("mousemove", mousemove)
+            .on("mouseleave", mouseleave);
+    });
 
 }
