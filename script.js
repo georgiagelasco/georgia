c202738b471158fa4b79aff4117c5c26a61ef003
georgiagelasco <124117294+georgiagelasco@users.noreply.github.com>
2024-12-10 16:44:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -134,18 +134,25 @@ function updateBarChart(attribute) {
             .on("click", function(event, d) {
                 var bar = d3.select(this);
                 var count = d[1];
-
-                alert("Count: " + count);
-
-                bar.attr("fill", "green");
-
-                svg.append("text")
-                    .attr("x", xScale(d[0]) + xScale.bandwidth() / 2)
-                    .attr("y", yScale(d[1]) - 10)
-                    .attr("text-anchor", "middle")
-                    .attr("fill", "black")
-                    .text(count);
+            
+                // Toggle the fill color between green and red
+                var isActive = bar.attr("fill") === "green";
+                var newColor = isActive ? "red" : "green";
+                bar.attr("fill", newColor);
+            
+                // Remove the previous text if "unclicked"
+                //bar.select("text").remove();
+            
+                if (!isActive) {
+                    svg.append("text")
+                        .attr("x", xScale(d[0]) + xScale.bandwidth() / 2)
+                        .attr("y", yScale(d[1]) - 10)
+                        .attr("text-anchor", "middle")
+                        .attr("fill", "black")
+                        .text(count);
+                }
             });
+
     });
 }
 
