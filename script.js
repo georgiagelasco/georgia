1a3cb9d580a96f19d42ffe66fcd7710396d79a84
georgiagelasco <124117294+georgiagelasco@users.noreply.github.com>
2024-12-10 23:19:43 -0500

    Update heatmAP script.js

--- script.js
+++ script.js
@@ -192,75 +192,85 @@ function updateBarChart(attribute) {
 
 // Update Heatmap
 function updateHeatmap() {
-    var margin = { top: 30, right: 30, bottom: 30, left: 30 },
+    // Set dimensions and margins
+    var margin = { top: 30, right: 30, bottom: 30, left: 100 },
         width = 450 - margin.left - margin.right,
-        height = 450 - margin.top - margin.bottom;
-
+        height = 650 - margin.top - margin.bottom; // Increased height
+    
+    // Append the svg object to the #heatmap container
     var svg = d3.select("#heatmap")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var myGroups = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
-    var myVars = ["v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10"];
-
-    var x = d3.scaleBand()
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+    // Read the data
+    d3.csv("covid.csv").then(function (data) {
+      // Labels for rows and columns
+      var myGroups = Array.from(new Set(data.map(d => d.age_group)));
+      var myVars = Array.from(new Set(data.map(d => d.race_ethnicity_combined)));
+    
+      // Build X scale and axis
+      var x = d3.scaleBand()
         .range([0, width])
         .domain(myGroups)
         .padding(0.01);
-    svg.append("g")
+      svg.append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x));
-
-    var y = d3.scaleBand()
+    
+      // Build Y scale and axis
+      var y = d3.scaleBand()
         .range([height, 0])
         .domain(myVars)
         .padding(0.01);
-    svg.append("g")
+      svg.append("g")
         .call(d3.axisLeft(y));
-
-    var myColor = d3.scaleLinear()
+    
+      // Build color scale
+      var myColor = d3.scaleLinear()
         .range(["white", "#69b3a2"])
-        .domain([1, 100]);
-
-    d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/heatmap_data.csv", function(data) {
-
-        var tooltip = d3.select("#my_dataviz")
-            .append("div")
-            .style("opacity", 0)
-            .attr("class", "tooltip")
-            .style("background-color", "white")
-            .style("border", "solid")
-            .style("border-width", "2px")
-            .style("border-radius", "5px")
-            .style("padding", "5px");
-
-        var mouseover = function(d) {
-            tooltip.style("opacity", 1);
-        };
-        var mousemove = function(d) {
-            tooltip
-                .html("The exact value of<br>this cell is: " + d.value)
-                .style("left", (d3.mouse(this)[0] + 70) + "px")
-                .style("top", (d3.mouse(this)[1]) + "px");
-        };
-        var mouseleave = function(d) {
-            tooltip.style("opacity", 0);
-        };
-
-        svg.selectAll()
-            .data(data, function(d) { return d.group + ':' + d.variable; })
-            .enter()
-            .append("rect")
-            .attr("x", function(d) { return x(d.group); })
-            .attr("y", function(d) { return y(d.variable); })
-            .attr("width", x.bandwidth())
-            .attr("height", y.bandwidth())
-            .style("fill", function(d) { return myColor(d.value); })
-            .on("mouseover", mouseover)
-            .on("mousemove", mousemove)
-            .on("mouseleave", mouseleave);
+        .domain([0, d3.max(data, d => +d.value)]);
+    
+      // Create tooltip
+      var tooltip = d3.select("body") // Append to body or the container element
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "2px")
+        .style("border-radius", "5px")
+        .style("padding", "5px");
+    
+      // Tooltip handlers
+      var mouseover = function (event, d) {
+        tooltip.style("opacity", 1);
+      };
+      var mousemove = function (event, d) {
+        tooltip
+          .html("The exact value of<br>this cell is: " + d.value)
+          .style("left", (event.pageX + 10) + "px")
+          .style("top", (event.pageY - 10) + "px");
+      };
+      var mouseleave = function () {
+        tooltip.style("opacity", 0);
+      };
+    
+      // Add squares
+      svg.selectAll()
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", d => x(d.age_group))
+        .attr("y", d => y(d.race_ethnicity_combined))
+        .attr("width", x.bandwidth())
+        .attr("height", y.bandwidth())
+        .style("fill", d => myColor(+d.value))
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave);
     });
+
 }
